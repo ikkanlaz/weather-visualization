18c510e0a5e276cb85fee5318d8c727bede07ef1
ikkanlaz <andrew.lazenby@gmail.com>
2018-05-20 14:10:43 -0700

    scaling graph

--- src/app/temperature-graph/temperature-graph.component.ts
+++ src/app/temperature-graph/temperature-graph.component.ts
@@ -12,6 +12,7 @@ export class TemperatureGraphComponent implements AfterContentInit {
 
   weatherData: object = alcatraz;
 
+
   constructor() { }
 
   ngAfterContentInit() {
@@ -19,15 +20,56 @@ export class TemperatureGraphComponent implements AfterContentInit {
   }
 
   buildTemperatureGraph() {
-    let vis = d3.select("#graph")
-      .append("svg")
-      .attr("width", 500)
-      .attr("height", 400);
-
+    let width = 800;
+    let height = 400;
     var nodes = this.getHourlyTemperatureData();
     var links = this.getLinks(nodes);
+    let startTime = this.getHourlyDataStartTime();
+    let endTime = this.getHourlyDataEndTime();
+    let lowestTemp = this.getLowestTemp() + 5;
+    let highestTemp = this.getHighestTemp() + 5;
 
-    vis.selectAll(".line")
+    let svg = d3.select("#graph")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    let plotMargins = {
+      top: 30,
+      bottom: 30,
+      left: 80,
+      right: 30
+    };
+    let plotGroup = svg.append('g')
+      .classed('plot', true)
+      .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`);
+
+    let plotWidth = width - plotMargins.left - plotMargins.right;
+    let plotHeight = height - plotMargins.top - plotMargins.bottom;
+
+    let xScale = d3.scaleTime()
+      .domain([startTime, endTime])
+      .nice()
+      .range([0, plotWidth]);
+    let xAxis = d3.axisBottom(xScale);
+    let xAxisGroup = plotGroup.append('g')
+      .classed('x', true)
+      .classed('axis', true)
+      .attr('transform', `translate(${0},${plotHeight})`)
+      .call(xAxis);
+
+    let yScale = d3.scaleLinear()
+      .domain([lowestTemp, highestTemp])
+      .range([plotHeight, 0]);
+    let yAxis = d3.axisLeft(yScale);
+    let yAxisGroup = plotGroup.append('g')
+      .classed('y', true)
+      .classed('axis', true)
+      .call(yAxis);
+
+
+
+    plotGroup.selectAll(".line")
       .data(links)
       .enter()
       .append("line")
@@ -59,6 +101,35 @@ export class TemperatureGraphComponent implements AfterContentInit {
       })
     }
     return links;
+  }
+
+  getHourlyDataStartTime() {
+    return moment.unix(this.weatherData["hourly"].data[0].time).toDate();
+  }
+
+  getHourlyDataEndTime() {
+    let data = this.weatherData["hourly"].data;
+    return moment.unix(data[data.length - 1].time).toDate();
+  }
+
+  getLowestTemp() {
+    let temp = 140;
+    this.weatherData["hourly"].data.forEach(node => {
+      if (node.temperature < temp) {
+        temp = node.temperature;
+      }
+    });
+    return temp;
+  }
+
+  getHighestTemp() {
+    let temp = -130;
+    this.weatherData["hourly"].data.forEach(node => {
+      if (node.temperature > temp) {
+        temp = node.temperature;
+      }
+    })
+    return temp;
   }
 
   getTimeDataRetrieved() {
